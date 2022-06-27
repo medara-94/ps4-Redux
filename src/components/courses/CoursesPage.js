d8c86d8169c46581074c44d26c7c1a89c309858a
Alex <bertoncin@bludata.com>
2022-06-27 16:57:25 +0200

    9 Redux SetUp and Configuration

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
-class CoursesPage extends React.Component {
-  render() {
-    return <h2>Courses</h2>;
-  }
-}
+const CoursesPage = () => {
+  const [course, setCourse] = useState({ course: { title: "" } });
+
+  const handleChange = (e) => {
+    const course2 = { ...course, title: e.target.value };
+    setCourse(course2);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(course.title);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Courses</h2>
+      <h3>Add Course</h3>
+      <input type="text" onChange={handleChange} value={course.title} />
+      <input type="submit" value="Save" />
+    </form>
+  );
+};
 
 export default CoursesPage;
